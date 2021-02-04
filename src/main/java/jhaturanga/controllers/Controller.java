2dac650e369f3cfb2580399e2ecff62915635d87
Alessandro Mazzoli <Alemazzoli97@gmail.com>
2021-02-04 16:27:04 +0100

    Added style formatter

--- src/main/java/jhaturanga/controllers/Controller.java
+++ src/main/java/jhaturanga/controllers/Controller.java
@@ -4,14 +4,14 @@ import jhaturanga.views.View;
 
 /**
  * 
- * @author Alessandro Mazzoli
  *
  */
 public interface Controller {
 
-	/**
-	 * Get the actual view of this controller.
-	 * @return the instance of the view attached to this controller.
-	 */
-	View getView();
+    /**
+     * Get the actual view of this controller.
+     * 
+     * @return the instance of the view attached to this controller.
+     */
+    View getView();
 }
