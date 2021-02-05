28d7ba1c783e6ef465632c173ceece576d343ec2
Alessandro Mazzoli <Alemazzoli97@gmail.com>
2021-02-05 22:34:21 +0100

    Added method in Game Controller

--- src/main/java/jhaturanga/model/game/GameController.java
+++ src/main/java/jhaturanga/model/game/GameController.java
@@ -1,5 +1,9 @@
 package jhaturanga.model.game;
 
+import java.util.Optional;
+
+import jhaturanga.model.player.Player;
+
 public interface GameController {
 
     /**
@@ -8,5 +12,18 @@ public interface GameController {
      * @return true if the game is completed, false otherwise
      */
     boolean isOver();
+    
+    /**
+     * Check if the game is ended with a draw.
+     * 
+     * @return true if the game ended in a draw, false otherwise
+     */
+    boolean isDraw();
 
+    /**
+     * Get the winner of this game if exist.
+     * 
+     * @return the winner of this game, if present
+     */
+    Optional<Player> getWinner();
 }
