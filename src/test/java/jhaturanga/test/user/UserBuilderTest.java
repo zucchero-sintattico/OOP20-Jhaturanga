90e1f6bcd2016f208f4e4e1f0584ce7c5cd1abcb
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2021-02-05 23:33:53 +0100

    Add UserBuilder tests

---
+++ src/test/java/jhaturanga/test/user/UserBuilderTest.java
@@ -0,0 +1,51 @@
+package jhaturanga.test.user;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import jhaturanga.model.user.UserBuilderImpl;
+import jhaturanga.model.user.UserImpl;
+import jhaturanga.model.user.User;
+import jhaturanga.model.user.UserBuilder;
+
+class UserBuilderTest {
+
+    private UserBuilder builder;
+    private static final String NAME = "Mario";
+
+    @BeforeEach
+    public void initBuilder() {
+        builder = new UserBuilderImpl(1, NAME);
+    }
+
+    @Test
+    void normalBuild() {
+        final var user = builder.winCount(3)
+                .drawCount(2)
+                .lostCount(1)
+                .build();
+        final User sameUser = new UserImpl(1, NAME, 3, 2, 1);
+        assertEquals(sameUser, user);
+    }
+
+    @Test
+    void emptyBuilder() {
+        final var user = builder.build();
+        final User sameUser = new UserImpl(1, NAME, 0, 0, 0);
+        assertEquals(sameUser, user);
+    }
+
+    @Test
+    void moreBuild() {
+        var user = builder.build();
+        final User sameUser = new UserImpl(1, NAME, 0, 0, 0);
+        assertEquals(sameUser, user);
+
+        user = builder.build();
+        assertNull(user);
+    }
+
+}
