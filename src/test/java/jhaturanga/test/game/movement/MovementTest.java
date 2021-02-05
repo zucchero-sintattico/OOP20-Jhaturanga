1b3cceabac167ee26ba7b7778f4f33c709d4cdd8
Alessandro Mazzoli <Alemazzoli97@gmail.com>
2021-02-05 22:58:26 +0100

    Added first test

---
+++ src/test/java/jhaturanga/test/game/movement/MovementTest.java
@@ -0,0 +1,14 @@
+package jhaturanga.test.game.movement;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MovementTest {
+
+    @Test
+    void test() {
+	fail("Not yet implemented");
+    }
+
+}
