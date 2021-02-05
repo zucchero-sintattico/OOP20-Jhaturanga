540a6f7d10fed0cbf766cb1677af7d5e1b64bbb7
Alessandro Mazzoli <Alemazzoli97@gmail.com>
2021-02-05 16:38:29 +0100

    Added getPieceAtPosition

--- src/main/java/jhaturanga/model/board/Board.java
+++ src/main/java/jhaturanga/model/board/Board.java
@@ -5,10 +5,19 @@ import java.util.Set;
 import jhaturanga.model.piece.Piece;
 
 public interface Board {
+
     /**
      * 
      * @return the state of the Match's Board as a set of Piece's
      * 
      */
     Set<Piece> getBoardState();
+
+    /**
+     * Get the piece in the specified position.
+     * 
+     * @param boardPosition - the position to check
+     * @return the piece in the specified position
+     */
+    Piece getPieceAtPosition(BoardPosition boardPosition);
 }
