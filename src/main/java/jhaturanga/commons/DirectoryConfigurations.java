f66f4bac7e1642d466a8989893299d98782c7b01
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2021-02-08 00:48:14 +0100

    Add more directory configurations

--- src/main/java/jhaturanga/commons/DirectoryConfigurations.java
+++ src/main/java/jhaturanga/commons/DirectoryConfigurations.java
@@ -12,8 +12,9 @@ import java.nio.file.Path;
 public final class DirectoryConfigurations {
 
     private static final String CONFIGURATION_DIRECTORY_NAME = ".jhaturanga";
+    private static final String USERS_DATA_DIRECTORY_NAME = "users_data";
+    private static final String USERS_DATA_NAME = "users.json";
     private static final String USER_DIRECTORY = System.getProperty("user.home");
-
     private static final String SEPARATOR = File.separator;
 
     /**
@@ -23,6 +24,22 @@ public final class DirectoryConfigurations {
             USER_DIRECTORY 
             + SEPARATOR 
             + CONFIGURATION_DIRECTORY_NAME;
+
+    /**
+     * Represent the path of the users data directory.
+     */
+    public static final String USERS_DATA_DIRECTORY_PATH = 
+            CONFIGURATION_DIRECTORY_PATH
+            + SEPARATOR
+            + USERS_DATA_DIRECTORY_NAME;
+
+    /**
+     * Represent the path of the users data file.
+     */
+    public static final String USERS_DATA_FILE_PATH =
+            USERS_DATA_DIRECTORY_PATH
+            + SEPARATOR
+            + USERS_DATA_NAME;
 
     private DirectoryConfigurations() {
     }
@@ -40,6 +57,42 @@ public final class DirectoryConfigurations {
 
         if (Files.notExists(Path.of(CONFIGURATION_DIRECTORY_PATH))) {
             Files.createDirectory(Path.of(CONFIGURATION_DIRECTORY_PATH));
+        }
+    }
+
+    /**
+     * This utility method will check if the data users directory exist,
+     * otherwise will create it.
+     * 
+     * @throws IOException
+     */
+    public static void validateUsersDataDirectory() throws IOException {
+        DirectoryConfigurations.validateInstallationDirectory();
+
+        if (!Files.isDirectory(Path.of(USERS_DATA_DIRECTORY_PATH))) {
+            Files.deleteIfExists(Path.of(USERS_DATA_DIRECTORY_PATH));
+        }
+
+        if (Files.notExists(Path.of(USERS_DATA_DIRECTORY_PATH))) {
+            Files.createDirectory(Path.of(USERS_DATA_DIRECTORY_PATH));
+        }
+    }
+
+    /**
+     * This utility method will check if the data users file exist,
+     * otherwise will create it.
+     * 
+     * @throws IOException
+     */
+    public static void validateUsersDataFile() throws IOException {
+        DirectoryConfigurations.validateUsersDataDirectory();
+
+        if (!Files.isRegularFile(Path.of(USERS_DATA_FILE_PATH))) {
+            Files.deleteIfExists(Path.of(USERS_DATA_FILE_PATH));
+        }
+
+        if (Files.notExists(Path.of(USERS_DATA_FILE_PATH))) {
+            Files.createFile(Path.of(USERS_DATA_FILE_PATH));
         }
     }
 }
