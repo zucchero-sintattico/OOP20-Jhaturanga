fd33eaae2aa3a20ecec691168d20721acff3133c
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2021-02-09 12:56:37 +0100

    Modify UsersManager interface

--- src/main/java/jhaturanga/model/user/management/UsersManager.java
+++ src/main/java/jhaturanga/model/user/management/UsersManager.java
@@ -1,5 +1,6 @@
 package jhaturanga.model.user.management;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.Optional;
 
@@ -19,8 +20,9 @@ public interface UsersManager {
      * @param password to login the game
      * @return the user identified by user name and password. If there is no user
      *         find, the Optional will be empty
+     * @throws IOException 
      */
-    Optional<User> login(String username, String password);
+    Optional<User> login(String username, String password) throws IOException;
 
     /**
      * 
@@ -29,12 +31,12 @@ public interface UsersManager {
      * @return the registered user. If the user name already exist, the Optional
      *         will be empty
      */
-    Optional<User> register(String username, String password);
+    Optional<User> register(String username, String password) throws IOException;
 
     /**
      * 
      * @return a {@link java.util.Collections} of all Users
      */
-    Collection<User> getAllUsers();
+    Collection<User> getAllUsers() throws IOException;
 
 }
