1e2285d7d223812bdf9b0ef7a9a14c9a06678b45
tommaso <tommaso.patriti@studio.unibo.it>
2021-02-10 23:17:29 +0100

    add fxml controller

--- src/main/java/jhaturanga/views/login/LoginViewImpl.java
+++ src/main/java/jhaturanga/views/login/LoginViewImpl.java
@@ -1,12 +1,18 @@
 package jhaturanga.views.login;
 
 import javafx.event.Event;
+import javafx.fxml.FXML;
 import javafx.stage.Stage;
 import jhaturanga.controllers.Controller;
 
 public final class LoginViewImpl implements LoginView {
 
     // private final LoginController controller;
+
+    @FXML
+    void initialize() {
+        System.out.println("cio");
+    }
 
     @Override
     public Controller getController() {
@@ -26,6 +32,7 @@ public final class LoginViewImpl implements LoginView {
         return null;
     }
 
+    @FXML
     @Override
     public void login(final Event event) {
         // TODO Auto-generated method stub
