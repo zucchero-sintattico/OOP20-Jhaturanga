71b7089c44188fdea0df4bd4e6fcbc6928f12685
tommaso <tommaso.patriti@studio.unibo.it>
2021-02-11 00:03:17 +0100

    add login view test implementation

--- src/main/java/jhaturanga/views/login/LoginViewImpl.java
+++ src/main/java/jhaturanga/views/login/LoginViewImpl.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import jhaturanga.controllers.Controller;
 import jhaturanga.controllers.login.LoginController;
@@ -12,6 +14,17 @@ import jhaturanga.controllers.login.LoginControllerImpl;
 public final class LoginViewImpl implements LoginView {
 
     private LoginController controller;
+
+    // declaration of element from fxml
+
+    @FXML
+    private TextField userNameTextField;
+
+    @FXML
+    private TextField passwordTextField;
+
+    @FXML
+    private Button loginButton;
 
     @FXML
     void initialize() {
@@ -44,8 +57,7 @@ public final class LoginViewImpl implements LoginView {
     @FXML
     @Override
     public void login(final Event event) {
-        // TODO Auto-generated method stub
-
+        System.out.println(this.controller.login(userNameTextField.getText(), passwordTextField.getText()));
     }
 
     @Override
