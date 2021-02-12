597bffbc37f816d28a63cbb095a02f06a147ff33
tommaso <tommaso.patriti@studio.unibo.it>
2021-02-12 13:14:14 +0100

    add Tiemr Interface

--- src/main/java/jhaturanga/model/timer/Timer.java
+++ src/main/java/jhaturanga/model/timer/Timer.java
@@ -1,8 +1,26 @@
 package jhaturanga.model.timer;
 
-/**
- * 
- */
-public interface Timer {
+import jhaturanga.model.player.Player;
 
+public interface Timer {
+    /**
+     * 
+     * @param player of which you want to know the remaining play time
+     * @return the remaining time of a player
+     */
+    int getRemaningTime(Player player);
+
+    /**
+     * 
+     * @param player which you want starting time
+     */
+    void start(Player player);
+
+    void switchPlayer();
+
+    boolean isMoficicable();
+
+    boolean updatePlayerTieme(Player player, int second);
+
+    void addTimeToPlayer(Player player, int seconds);
 }
