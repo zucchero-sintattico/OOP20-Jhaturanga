1520d9b1bc21a8cf601c4ae47263d9061f72d874
StefanoScolari <stefano.scolari@studio.unibo.it>
2021-02-15 23:12:25 +0100

    implemented turns/rounds in the game logic

---
+++ src/test/java/jhaturanga/text/game/matchturns/MatchTurnsTest.java
@@ -0,0 +1,54 @@
+package jhaturanga.text.game.matchturns;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import jhaturanga.model.board.BoardPositionImpl;
+import jhaturanga.model.game.ClassicGameType;
+import jhaturanga.model.match.Match;
+import jhaturanga.model.match.builder.MatchBuilder;
+import jhaturanga.model.match.builder.MatchBuilderImpl;
+import jhaturanga.model.movement.MovementImpl;
+import jhaturanga.model.player.Player;
+import jhaturanga.model.player.PlayerColor;
+import jhaturanga.model.player.PlayerImpl;
+
+class MatchTurnsTest {
+
+    private Player whitePlayer;
+    private Player blackPlayer;
+
+    @BeforeEach
+    public void init() {
+        this.whitePlayer = new PlayerImpl(PlayerColor.WHITE);
+        this.blackPlayer = new PlayerImpl(PlayerColor.BLACK);
+    }
+
+    @Test
+    void testMatchPlayersTurns() {
+        final MatchBuilder matchBuilder = new MatchBuilderImpl();
+
+        final Match match = matchBuilder.gameType(new ClassicGameType(this.whitePlayer, this.blackPlayer))
+                .players(List.of(this.whitePlayer, this.blackPlayer)).build();
+
+        /**
+         * So at this point White player should be starting
+         */
+
+        assertTrue(match.move(new MovementImpl(match.getBoard().getPieceAtPosition(new BoardPositionImpl(0, 1)).get(),
+                new BoardPositionImpl(0, 3))));
+        /**
+         * I try moving a piece of the same player (in this case White Player's), it
+         * should return false and not let me do the move
+         */
+
+        assertFalse(match.move(new MovementImpl(match.getBoard().getPieceAtPosition(new BoardPositionImpl(1, 1)).get(),
+                new BoardPositionImpl(1, 3))));
+
+    }
+}
