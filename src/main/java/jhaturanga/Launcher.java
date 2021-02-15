6b07665fe6491f8cad2697cc408992269dd4db20
Alessandro Mazzoli <Alemazzoli97@gmail.com>
2021-02-15 22:08:28 +0100

    Refactor launcher

--- src/main/java/jhaturanga/Launcher.java
+++ src/main/java/jhaturanga/Launcher.java
@@ -1,5 +1,39 @@
 package jhaturanga;
 
-public final class Launcher {
+import java.io.IOException;
 
+import jhaturanga.views.login.*;
+
+public final class Launcher {
+    
+    private static final String COMMAND_LINE_PARAMETER = "-cmd";
+
+    private Launcher() {
+
+    }
+
+    private static void startCommandLine() throws IOException {
+        //final CommandLineLoginView view = new CommandLineLoginView();
+        //final LoginController loginController = new LoginControllerImpl(view);
+        //view.setController(loginController);
+        //view.run();
+    }
+
+    private static void startJavaFx(final String[] args) {
+        //Jhaturanga.main(args);
+    }
+
+    public static void main(final String[] args) throws IOException {
+
+        if (args.length > 0) {
+            if (args[0].equals(COMMAND_LINE_PARAMETER)) {
+                startCommandLine();
+            }
+        } else {
+            startJavaFx(args);
+        }
+
+    }
+
+}
 }
