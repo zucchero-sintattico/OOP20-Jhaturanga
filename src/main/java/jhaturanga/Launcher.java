27e571697a5955be1dec2e72f266ef4babaaec11
Alessandro Mazzoli <Alemazzoli97@gmail.com>
2021-02-15 22:01:18 +0100

    Fix conflict

--- src/main/java/jhaturanga/Launcher.java
+++ src/main/java/jhaturanga/Launcher.java
@@ -3,7 +3,6 @@ package jhaturanga;
 import java.io.IOException;
 
 import jhaturanga.controllers.login.LoginController;
-import jhaturanga.controllers.login.LoginControllerImpl;
 import jhaturanga.views.login.CommandLineLoginView;
 
 public final class Launcher {
--- src/main/java/jhaturanga/controllers/login/LoginControllerImpl.java
+++ src/main/java/jhaturanga/controllers/login/LoginControllerImpl.java
@@ -3,21 +3,20 @@ package jhaturanga.controllers.login;
 import java.io.IOException;
 import java.util.Optional;
 
+import jhaturanga.commons.datastorage.UsersDataStorageJsonImpl;
 import jhaturanga.model.user.User;
-import jhaturanga.model.user.management.UsersManager;
-import jhaturanga.model.user.management.UsersManagerJsonImpl;
+import jhaturanga.model.user.management.UsersManagerFacade;
+import jhaturanga.model.user.management.UsersManagerImpl;
 import jhaturanga.views.View;
 import jhaturanga.views.login.LoginView;
 
 public final class LoginControllerImpl implements LoginController {
 
-    private UsersManager userManager;
+    private final UsersManagerFacade userManager;
     private final LoginView view;
 
     public LoginControllerImpl(final LoginView view) throws IOException {
-
-        this.userManager = new UsersManagerJsonImpl();
-
+        this.userManager = new UsersManagerImpl(new UsersDataStorageJsonImpl());
         this.view = view;
     }
 
