4e2ef116f25e9d8a7e4ed4b2da2d91e717ac64de
tommaso <tommaso.patriti@studio.unibo.it>
2021-02-16 00:14:00 +0100

    Emum Bug ApplicationStyle ToDo: fix it

--- src/main/java/jhaturanga/commons/style/ApplicationStyle.java
+++ src/main/java/jhaturanga/commons/style/ApplicationStyle.java
@@ -2,15 +2,13 @@ package jhaturanga.commons.style;
 
 public final class ApplicationStyle {
 
-    public enum ApplicationStyleEnum {
-        LIGHT, DARK
+    private ApplicationStyle() {
     }
 
     private static ApplicationStyleEnum currentStyle = ApplicationStyleEnum.LIGHT;
 
     public static void setApplicationStyle(final ApplicationStyleEnum style) {
         currentStyle = style;
-
     }
 
     public ApplicationStyleEnum getApplicationStyle() {
@@ -18,13 +16,27 @@ public final class ApplicationStyle {
     }
 
     public static String getApplicationStylePath() {
-        // TODO Auto-generated method stub
-        return "css/dark.css";
+        return currentStyle.path;
     }
 
-    public String getApplicationStylePath(final ApplicationStyleEnum style) {
-        // TODO Auto-generated method stub
-        return "css/dark.css";
+    public static String getApplicationStylePath(final ApplicationStyleEnum style) {
+        return style.path;
     }
 
-}
+    enum ApplicationStyleEnum {
+        /**
+         * light style.
+         */
+        LIGHT("css/light.css"),
+        /**
+         * dark style.
+         */
+        DARK("css/dark.css");
+
+        private final String path;
+
+        ApplicationStyleEnum(final String path) {
+            this.path = path;
+        }
+    }
+}​ ​
