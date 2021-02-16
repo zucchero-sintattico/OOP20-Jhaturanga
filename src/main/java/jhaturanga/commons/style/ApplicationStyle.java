2aab63def582696bf2f23653089de3a71035540e
Ro0t-set <ro0trexustom@gmail.com>
2021-02-16 11:53:45 +0100

    add javadoc

--- src/main/java/jhaturanga/commons/style/ApplicationStyle.java
+++ src/main/java/jhaturanga/commons/style/ApplicationStyle.java
@@ -6,19 +6,36 @@ public final class ApplicationStyle {
     }
 
     private static ApplicationStyleEnum currentStyle = ApplicationStyleEnum.LIGHT;
-
+    
+    /**
+     * 
+     * @param style witch want set
+     */
     public static void setApplicationStyle(final ApplicationStyleEnum style) {
         currentStyle = style;
     }
-
+    
+    /**
+     * 
+     * @return current application style
+     */
     public ApplicationStyleEnum getApplicationStyle() {
         return currentStyle;
     }
 
+    /**
+     * 
+     * @return path of current application style
+     */
     public static String getApplicationStylePath() {
         return currentStyle.getPath();
     }
 
+    /**
+     * 
+     * @param style witch want have path
+     * @return the path of the selection style
+     */
     public static String getApplicationStylePath(final ApplicationStyleEnum style) {
         return style.getPath();
     }
--- src/main/java/jhaturanga/views/PageLoader.java
+++ src/main/java/jhaturanga/views/PageLoader.java
@@ -16,6 +16,12 @@ public final class PageLoader {
     private PageLoader() {
     }
 
+    /**
+     * 
+     * @param stage to load
+     * @param page to load
+     * @throws IOException if file not found
+     */
     public static void switchPage(final Stage stage, final String page) throws IOException {
         final FXMLLoader loader = new FXMLLoader(ClassLoader.getSystemResource(PATH_START + page + PATH_END));
         final Parent root = loader.load();
@@ -26,12 +32,21 @@ public final class PageLoader {
         stage.getScene().getStylesheets().add(ApplicationStyle.getApplicationStylePath());
         stage.show();
     }
-
+    
+    /**
+     * 
+     * @param page page to load
+     * @throws IOException  if file not found
+     */
     public static void newPage(final String page) throws IOException {
         final Stage stage = new Stage();
         switchPage(stage, page);
     }
-
+    
+    /**
+     * 
+     * @param stage to load
+     */
     public static void updatePage(final Stage stage) {
         stage.getScene().getStylesheets().clear();
         stage.getScene().getStylesheets().add(ApplicationStyle.getApplicationStylePath());
