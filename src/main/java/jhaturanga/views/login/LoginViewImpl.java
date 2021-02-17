3320584461d09347600572f9c620a2013899366a
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2021-02-17 11:55:39 +0100

    Implement Validator in the login

--- src/main/java/jhaturanga/views/login/LoginViewImpl.java
+++ src/main/java/jhaturanga/views/login/LoginViewImpl.java
@@ -10,20 +10,22 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import jhaturanga.commons.validator.ValidatorBuilder;
 import jhaturanga.commons.validator.ValidatorBuilderImpl;
 import jhaturanga.controllers.Controller;
 import jhaturanga.controllers.login.LoginController;
 import jhaturanga.controllers.login.LoginControllerImpl;
+import jhaturanga.model.user.management.UsersManager;
 import jhaturanga.views.PageLoader;
+import jhaturanga.commons.validator.ValidatorBuilder.ValidationResult;
+import static jhaturanga.commons.validator.ValidatorBuilder.ValidationResult.CORRECT;
 
 public final class LoginViewImpl implements LoginView {
 
     private Stage stage;
 
     private LoginController controller;
-    private ValidatorBuilder validetor;
-    private Function<String, ValidatorBuilder.ValidationResult> fieldValidator;
+    private Function<String, ValidationResult> passwordValidator;
+    private Function<String, ValidationResult> usernameValidator;
 
     // declaration of element from fxml
 
@@ -46,8 +48,19 @@ public final class LoginViewImpl implements LoginView {
 
     @FXML
     void initialize() {
-        this.validetor = new ValidatorBuilderImpl();
-        this.fieldValidator = new ValidatorBuilderImpl().notEmpty().notLongerThan(3).notLongerThan(20).build();
+        this.passwordValidator = new ValidatorBuilderImpl()
+                .notEmpty()
+                .notShortedThan(3)
+                .notLongerThan(16)
+                .build();
+
+        this.usernameValidator = new ValidatorBuilderImpl()
+                .notEmpty()
+                //.notShortedThan(5)
+                .notLongerThan(32)
+                .forbid(UsersManager.GUEST.getUsername())
+                .build();
+
         try {
             this.controller = new LoginControllerImpl(this);
         } catch (IOException e) {
@@ -85,21 +98,38 @@ public final class LoginViewImpl implements LoginView {
     @FXML
     @Override
     public void login(final Event event) {
+        errorText.setText("");
+        final ValidationResult passwordResult = this.passwordValidator.apply(passwordTextField.getText());
 
-        this.controller.login(userNameTextField.getText(), passwordTextField.getText());
-        System.out.println("accesso consentito");
+        if (passwordResult == CORRECT) {
+            this.controller.login(userNameTextField.getText(), passwordTextField.getText());
+            System.out.println("accesso consentito");
+        } else {
+            errorText.setText(passwordResult.getMessage() + " password");
+        }
 
     }
 
     @Override
     public void register(final Event event) {
-        this.controller.register(userNameTextField.getText(), passwordTextField.getText());
+        errorText.setText("");
+        final ValidationResult passwordResult = this.passwordValidator.apply(passwordTextField.getText());
+        final ValidationResult usernameResult = this.usernameValidator.apply(userNameTextField.getText());
 
-        try {
-            PageLoader.switchPage(this.getStage(), "login");
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        if (usernameResult == CORRECT) {
+            if (passwordResult == CORRECT) {
+                this.controller.register(userNameTextField.getText(), passwordTextField.getText());
+                try {
+                    PageLoader.switchPage(this.getStage(), "login");
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            } else {
+                errorText.setText(passwordResult.getMessage() + " password");
+            }
+        } else {
+            errorText.setText(usernameResult.getMessage() + " username");
         }
 
     }
