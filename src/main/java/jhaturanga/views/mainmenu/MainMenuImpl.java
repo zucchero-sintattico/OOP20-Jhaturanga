518ecfe7e0e3ec0eb03cdf04706be4c263d91697
tommaso <tommaso.patriti@studio.unibo.it>
2021-02-17 15:07:35 +0100

    gameModeChoices implementation

--- src/main/java/jhaturanga/views/mainmenu/MainMenuImpl.java
+++ src/main/java/jhaturanga/views/mainmenu/MainMenuImpl.java
@@ -1,14 +1,25 @@
 package jhaturanga.views.mainmenu;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 import jhaturanga.controllers.Controller;
 import jhaturanga.controllers.login.LoginController;
+import jhaturanga.model.game.gametypes.GameTypesEnum;
 
 public final class MainMenuImpl implements MainMenu {
 
     private Stage stage;
-
     private LoginController controller;
+
+    @FXML
+    private ChoiceBox<GameTypesEnum> gameModeChoices;
+
+    @FXML
+    void initialize() {
+        gameModeChoices.getItems().addAll(GameTypesEnum.values());
+        gameModeChoices.setValue(GameTypesEnum.CLASSIC_GAME);
+    }
 
     @Override
     public Controller getController() {
