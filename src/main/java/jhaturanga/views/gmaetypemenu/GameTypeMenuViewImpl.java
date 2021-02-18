a8f4514fe6ff99284555272e574e1aac509c361e
tommaso <tommaso.patriti@studio.unibo.it>
2021-02-18 22:45:05 +0100

    grid fix

--- src/main/java/jhaturanga/views/gmaetypemenu/GameTypeMenuViewImpl.java
+++ src/main/java/jhaturanga/views/gmaetypemenu/GameTypeMenuViewImpl.java
@@ -1,8 +1,8 @@
 package jhaturanga.views.gmaetypemenu;
 
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import jhaturanga.controllers.gametypemenu.GameTypeMenuController;
 import jhaturanga.model.game.gametypes.GameTypesEnum;
@@ -11,7 +11,9 @@ import jhaturanga.views.AbstractView;
 public final class GameTypeMenuViewImpl extends AbstractView implements GameTypeMenuView {
 
     @FXML
-    private GridPane gameGrid;
+    private AnchorPane gameGrid;
+
+    private final GridPane grid = new GridPane();
 
     @FXML
     void inizialize() {
@@ -20,19 +22,32 @@ public final class GameTypeMenuViewImpl extends AbstractView implements GameType
 
     @Override
     public void init() {
-        gameGrid.setAlignment(Pos.CENTER);
+        gameGrid.getChildren().add(grid);
 
-        for (int x = 0; x < this.getGameTypeMenuController().getNumberOfColumn(); x++) {
-            for (int y = 0; y < this.getGameTypeMenuController().getNumberOfRow(); y++) {
+        int enumCounter = 0;
+        for (int y = 0; y < this.getGameTypeMenuController().getNumberOfColumn(); y++) {
+            for (int x = 0; x < this.getGameTypeMenuController().getNumberOfRow(); x++) {
+
                 final Button button = new Button();
-                button.setText(GameTypesEnum.values()[x].toString());
-                gameGrid.add(new Button(), x, y, 1, 1);
+
+                button.setText(GameTypesEnum.values()[enumCounter].toString());
+                enumCounter++;
+
+                grid.add(button, x, y, 1, 1);
+
             }
 
         }
+        if (enumCounter < this.getGameTypeMenuController().getnNumbersOfGameTipes()) {
+            final Button button = new Button();
+            button.setText(GameTypesEnum.values()[enumCounter].toString());
+            grid.add(button, this.getGameTypeMenuController().getNumberOfColumn() + 1, 0);
+        }
 
-        Button button = new Button();
-        gameGrid.add(button, 0, 0, 1, 1);
+        grid.setGridLinesVisible(true);
+        grid.prefWidthProperty().bind(gameGrid.widthProperty());
+        grid.prefHeightProperty().bind(gameGrid.heightProperty());
+
     }
 
     @Override
