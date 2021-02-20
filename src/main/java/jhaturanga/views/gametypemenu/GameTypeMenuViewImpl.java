dd0ed022ee3a816dcf5dff68054cf8ec8b0d4b7e
StefanoScolari <stefano.scolari@studio.unibo.it>
2021-02-20 11:53:22 +0100

    fixed tom's typos

--- src/main/java/jhaturanga/views/gametypemenu/GameTypeMenuViewImpl.java
+++ src/main/java/jhaturanga/views/gametypemenu/GameTypeMenuViewImpl.java
@@ -1,4 +1,4 @@
-package jhaturanga.views.gmaetypemenu;
+package jhaturanga.views.gametypemenu;
 
 import java.util.Arrays;
 import java.util.Iterator;
@@ -54,8 +54,6 @@ public final class GameTypeMenuViewImpl extends AbstractView implements GameType
             }
 
         }
-
-        // grid.setGridLinesVisible(true);
 
         gameGrid.getChildren().add(grid);
 
