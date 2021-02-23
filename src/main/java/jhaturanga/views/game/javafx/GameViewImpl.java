cd9d06589904e4756b88360437edd3345ac3c127
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2021-02-23 10:41:44 +0100

    Add mainPane and grid for game

--- src/main/java/jhaturanga/views/game/javafx/GameViewImpl.java
+++ src/main/java/jhaturanga/views/game/javafx/GameViewImpl.java
@@ -2,6 +2,7 @@ package jhaturanga.views.game.javafx;
 
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import jhaturanga.controllers.game.GameController;
 import jhaturanga.views.AbstractView;
 import jhaturanga.views.board.BoardView;
@@ -10,12 +11,15 @@ import jhaturanga.views.game.GameView;
 public final class GameViewImpl extends AbstractView implements GameView {
 
     @FXML
-    private AnchorPane mainAnchorPane;
+    private AnchorPane root;
+
+    @FXML
+    private GridPane grid;
 
     @FXML
     public void initialize() {
-        this.mainAnchorPane.getChildren()
-                .add(new BoardView(this.mainAnchorPane.widthProperty(), this.mainAnchorPane.heightProperty(), 8, 8));
+        this.root.getChildren()
+                .add(new BoardView(this.root.widthProperty(), this.root.heightProperty(), 8, 8));
     }
 
     @Override
