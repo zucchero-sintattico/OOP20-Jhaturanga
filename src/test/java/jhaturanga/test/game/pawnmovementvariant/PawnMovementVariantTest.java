9d25378c56b8f1aaa245acca50456abd42fa3dd2
Alessandro Mazzoli <Alemazzoli97@gmail.com>
2021-03-07 11:03:11 +0100

    Removed warning from pawn movement variant test

--- src/test/java/jhaturanga/test/game/pawnmovementvariant/PawnMovementVariantTest.java
+++ src/test/java/jhaturanga/test/game/pawnmovementvariant/PawnMovementVariantTest.java
@@ -16,6 +16,7 @@ import jhaturanga.model.player.Player;
 import jhaturanga.model.player.PlayerColor;
 import jhaturanga.model.player.PlayerImpl;
 import jhaturanga.model.user.management.UsersManager;
+import jhaturanga.test.commons.Constants;
 
 class PawnMovementVariantTest {
 
@@ -45,16 +46,19 @@ class PawnMovementVariantTest {
          */
 
         // White pawn doing top right move
-        assertTrue(match.move(new MovementImpl(match.getBoard().getPieceAtPosition(new BoardPositionImpl(2, 1)).get(),
-                new BoardPositionImpl(3, 2))));
+        assertTrue(match.move(new MovementImpl(
+                match.getBoard().getPieceAtPosition(new BoardPositionImpl(Constants.TWO, Constants.ONE)).get(),
+                new BoardPositionImpl(Constants.THREE, Constants.TWO))));
 
         // Black pawn can't do double-step forward move
-        assertFalse(match.move(new MovementImpl(match.getBoard().getPieceAtPosition(new BoardPositionImpl(1, 6)).get(),
-                new BoardPositionImpl(1, 4))));
+        assertFalse(match.move(new MovementImpl(
+                match.getBoard().getPieceAtPosition(new BoardPositionImpl(Constants.ONE, Constants.SIX)).get(),
+                new BoardPositionImpl(Constants.ONE, Constants.FOUR))));
 
         // Let's move black pawn bottom left
-        assertTrue(match.move(new MovementImpl(match.getBoard().getPieceAtPosition(new BoardPositionImpl(1, 6)).get(),
-                new BoardPositionImpl(0, 5))));
+        assertTrue(match.move(new MovementImpl(
+                match.getBoard().getPieceAtPosition(new BoardPositionImpl(Constants.ONE, Constants.SIX)).get(),
+                new BoardPositionImpl(Constants.ZERO, Constants.FIVE))));
     }
 
 }
