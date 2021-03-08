e67d994dac15b336026ec9bdfcf1671d991d2f81
Tommaso Patriti <tommasopatriti@192.168.1.14>
2021-03-08 14:26:02 +0100

    comments of Home controller interface

--- src/main/java/jhaturanga/controllers/home/HomeController.java
+++ src/main/java/jhaturanga/controllers/home/HomeController.java
@@ -36,26 +36,68 @@ public interface HomeController extends Controller {
      */
     void createMatch();
 
-//TODO: TOMMASO DOCUMENTA
-
+    /**
+     * 
+     * @return name of selected game type.
+     */
     Optional<String> getNameGameTypeSelected();
 
+    /**
+     * Set white player.
+     * 
+     * @param player
+     */
     void setWhitePlayer(Player player);
 
+    /**
+     * Set black player.
+     * 
+     * @param player
+     */
     void setBlackPlayer(Player player);
 
+    /**
+     * first player logged.
+     * 
+     * @return true if fist player is logged, else false.
+     */
     boolean isFirstUserLogged();
 
+    /**
+     * second player logged.
+     * 
+     * @return true if second player is second, else false.
+     */
     boolean isSecondUserLogged();
 
+    /**
+     * set the fist player as GUEST.
+     */
     void setFirstUserGuest();
 
+    /**
+     * set the second player as GUEST.
+     */
     void setSecondUserGuest();
 
+    /**
+     * 
+     * @return fist User logged
+     */
     User getFirstUser();
 
+    /**
+     * 
+     * @return second User logged
+     */
     User getSecondUser();
 
+    /**
+     * 
+     * @return List containing board
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
     List<Board> loadMatch() throws IOException, ClassNotFoundException;
 
 }
