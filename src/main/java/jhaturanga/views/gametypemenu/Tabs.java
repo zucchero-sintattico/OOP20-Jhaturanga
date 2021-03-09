43136b09cb52bc275f72dfa4cc006aef386a535f
StefanoScolari <stefano.scolari@studio.unibo.it>
2021-03-09 21:44:03 +0100

    improved gui

--- src/main/java/jhaturanga/views/gametypemenu/Tabs.java
+++ src/main/java/jhaturanga/views/gametypemenu/Tabs.java
@@ -12,30 +12,24 @@ public class Tabs extends VBox {
     private final TextArea description;
     private final Button button;
 
-    public Tabs(final ObservableDoubleValue width, final ObservableDoubleValue height, final int numberOfTab) {
+    public Tabs(final ObservableDoubleValue width, final ObservableDoubleValue heigth, final int numberOfTab) {
 
         this.description = new TextArea();
         this.button = new Button();
         this.description.setEditable(false);
         this.setPadding(new Insets(10));
-        // this.setAlignment(Pos.BOTTOM_CENTER);
 
         this.prefWidthProperty().bind(Bindings.divide(width, numberOfTab / 2));
-        this.prefHeightProperty().bind(Bindings.divide(height, numberOfTab / 2));
+        this.prefHeightProperty().bind(Bindings.divide(heigth, numberOfTab / 2));
         if (numberOfTab % 2 != 0) {
             this.prefWidthProperty().bind(Bindings.divide(width, numberOfTab / 2 + 1));
         }
 
-        this.button.prefWidthProperty().bind(this.widthProperty());
-        this.button.prefHeightProperty().bind(this.heightProperty().divide(5));
-        this.description.prefHeightProperty().bind(this.heightProperty().divide(2));
-        this.description.prefWidthProperty().bind(this.widthProperty());
+        this.button.prefWidthProperty().bind(width);
+        this.button.prefHeightProperty().bind(Bindings.divide(heigth, 5));
+        this.description.prefHeightProperty().bind(Bindings.divide(heigth, 2));
+        this.description.prefWidthProperty().bind(width);
 
-        this.description.styleProperty()
-                .bind(Bindings.concat("-fx-font-size: ", this.widthProperty().add(this.widthProperty()).divide(30)));
-
-        this.button.styleProperty()
-                .bind(Bindings.concat("-fx-font-size: ", this.widthProperty().add(this.widthProperty()).divide(30)));
         this.getChildren().addAll(description, button);
 
     }
