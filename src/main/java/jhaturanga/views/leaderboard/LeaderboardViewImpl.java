52642a033c85f8d4eff88089452dc3c4f0a3cc40
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2021-03-17 00:01:19 +0100

    Finish first version of leaderboard

--- src/main/java/jhaturanga/views/leaderboard/LeaderboardViewImpl.java
+++ src/main/java/jhaturanga/views/leaderboard/LeaderboardViewImpl.java
@@ -1,12 +1,14 @@
 package jhaturanga.views.leaderboard;
 
 import java.io.IOException;
-
+import com.sun.javafx.collections.ObservableListWrapper;
+import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import jhaturanga.controllers.leaderboard.LeaderboardController;
+import jhaturanga.model.user.User;
 import jhaturanga.pages.PageLoader;
 import jhaturanga.pages.Pages;
 import jhaturanga.views.AbstractView;
@@ -14,11 +16,33 @@ import jhaturanga.views.AbstractView;
 public final class LeaderboardViewImpl extends AbstractView implements LeaderboardView {
 
     @FXML
-    private TableView<String> table;
+    private TableView<User> table;
 
     @Override
     public void init() {
 
+        final TableColumn<User, String> nameColumn = new TableColumn<>("user");
+        nameColumn.setCellValueFactory(u -> new ReadOnlyStringWrapper(u.getValue().getUsername()));
+
+        final TableColumn<User, String> winColumn = new TableColumn<>("win");
+        winColumn.setCellValueFactory(u -> new ReadOnlyStringWrapper(String.valueOf(u.getValue().getWinCount())));
+
+        final TableColumn<User, String> loseColumn = new TableColumn<>("lose");
+        loseColumn.setCellValueFactory(u -> new ReadOnlyStringWrapper(String.valueOf(u.getValue().getLostCount())));
+
+        final TableColumn<User, String> drawColumn = new TableColumn<>("draw");
+        drawColumn.setCellValueFactory(u -> new ReadOnlyStringWrapper(String.valueOf(u.getValue().getDrawCount())));
+
+        table.getColumns().add(nameColumn);
+        table.getColumns().add(winColumn);
+        table.getColumns().add(loseColumn);
+        table.getColumns().add(drawColumn);
+        try {
+            this.table.setItems(new ObservableListWrapper<>(this.getLeaderboardController().getUsers()));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -31,4 +55,3 @@ public final class LeaderboardViewImpl extends AbstractView implements Leaderboa
         PageLoader.switchPage(this.getStage(), Pages.HOME, this.getController().getModel());
     }
 }
-
