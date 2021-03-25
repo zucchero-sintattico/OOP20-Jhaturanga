7ac8524fd8a06dfe372dfe56c6b8f6ce5eb4f5c7
Tommaso Patriti <tommaso.patriti@studio.unibo.it>
2021-03-26 00:07:11 +0100

    refactoring

--- src/main/java/jhaturanga/views/pages/PageLoader.java
+++ src/main/java/jhaturanga/views/pages/PageLoader.java
@@ -24,8 +24,8 @@ public final class PageLoader {
     }
 
     private static void loadStyle(final Stage stage) {
-        stage.setMinHeight(0.0);
-        stage.setMinWidth(0.0);
+//        stage.setMinHeight(500);
+//        stage.setMinWidth(500);
         stage.getScene().getStylesheets().clear();
         stage.getScene().getStylesheets().add(ApplicationStyle.getApplicationStylePath());
     }
@@ -63,6 +63,7 @@ public final class PageLoader {
         final JavaFXView view = loader.getController();
 
         final Controller controller = page.getNewControllerInstance();
+
         controller.setApplicationInstance(applicationInstance);
         controller.setView(view);
 
@@ -75,7 +76,7 @@ public final class PageLoader {
         fadeIn.setToValue(1.0);
         fadeIn.play();
         stage.show();
-        //stage.centerOnScreen();
+        // stage.centerOnScreen();
     }
 
     /**
