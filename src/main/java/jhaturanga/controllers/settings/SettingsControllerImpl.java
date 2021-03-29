ac1014e544e16292478cf610cfffcb17f22040c7
Tommaso Patriti <tommaso.patriti@studio.unibo.it>
2021-03-29 12:50:32 +0200

    refatoring setting controler, try/catch managemant

--- src/main/java/jhaturanga/controllers/settings/SettingsControllerImpl.java
+++ src/main/java/jhaturanga/controllers/settings/SettingsControllerImpl.java
@@ -1,30 +1,52 @@
 package jhaturanga.controllers.settings;
 
+import java.io.IOException;
+
 import jhaturanga.commons.style.ApplicationStyle;
 import jhaturanga.commons.style.ApplicationStyleEnum;
 import jhaturanga.commons.style.PieceStyle;
 import jhaturanga.commons.style.PieceStyleEnum;
+import jhaturanga.commons.style.StyleSettingManager;
 import jhaturanga.controllers.AbstractController;
 
 public final class SettingsControllerImpl extends AbstractController implements SettingsController {
 
     @Override
     public void setApplicationStyle(final ApplicationStyleEnum style) {
-        ApplicationStyle.setApplicationStyle(style);
+        try {
+            StyleSettingManager.setAndSaveApplicationStyle(style);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
     public ApplicationStyleEnum getCurrentApplicationStyle() {
+        try {
+            return StyleSettingManager.getSavedApplicatioStyle();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return ApplicationStyle.getApplicationStyle();
     }
 
     @Override
     public void setPlayerStyle(final PieceStyleEnum style) {
-        PieceStyle.setPieceStyle(style);
+        try {
+            StyleSettingManager.setAndSavePieceStyle(style);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public PieceStyleEnum getCurrentPlayerStyle() {
+        try {
+            return StyleSettingManager.getSavedPieceStyle();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return PieceStyle.getPieceStyle();
     }
 
--- src/main/java/jhaturanga/views/settings/SettingsViewImpl.java
+++ src/main/java/jhaturanga/views/settings/SettingsViewImpl.java
@@ -1,14 +1,12 @@
 package jhaturanga.views.settings;
 
-import java.io.IOException;
-
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import jhaturanga.commons.style.ApplicationStyleEnum;
 import jhaturanga.commons.style.PieceStyleEnum;
-import jhaturanga.commons.style.StyleSettingManager;
+import jhaturanga.controllers.settings.SettingsController;
 import jhaturanga.views.AbstractJavaFXView;
 import jhaturanga.views.pages.PageLoader;
 import jhaturanga.views.pages.Pages;
@@ -21,18 +19,18 @@ public final class SettingsViewImpl extends AbstractJavaFXView {
     @FXML
     private ChoiceBox<PieceStyleEnum> piecesListChoiceBox;
 
+    private SettingsController getSettingController() {
+        return (SettingsController) this.getController();
+    }
+
     @Override
     public void init() {
         this.styleListChoiceBox.getItems().addAll(ApplicationStyleEnum.values());
         this.piecesListChoiceBox.getItems().addAll(PieceStyleEnum.values());
 
-        try {
-            this.styleListChoiceBox.setValue(StyleSettingManager.getSavedApplicatioStyle());
-            this.piecesListChoiceBox.setValue(StyleSettingManager.getSavedPieceStyle());
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        this.styleListChoiceBox.setValue(this.getSettingController().getCurrentApplicationStyle());
+        this.piecesListChoiceBox.setValue(this.getSettingController().getCurrentPlayerStyle());
+
 //        this.getStage().setMinHeight(this.getStage().getHeight());
 //        this.getStage().setMinWidth(this.getStage().getWidth());
 
@@ -49,13 +47,10 @@ public final class SettingsViewImpl extends AbstractJavaFXView {
 
     @FXML
     public void saveButton(final Event event) {
-        try {
-            StyleSettingManager.setAndSaveApplicationStyle(this.styleListChoiceBox.getValue());
-            StyleSettingManager.setAndSavePieceStyle(this.piecesListChoiceBox.getValue());
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+
+        this.getSettingController().setApplicationStyle(this.styleListChoiceBox.getValue());
+        this.getSettingController().setPlayerStyle(this.piecesListChoiceBox.getValue());
+
         PageLoader.switchPage(this.getStage(), Pages.SETTINGS, this.getController().getApplicationInstance());
 
     }
