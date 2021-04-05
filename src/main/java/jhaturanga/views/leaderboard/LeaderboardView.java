2b794f3682c8da08398eca706b86b79f85308ce0
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2021-04-06 00:05:02 +0200

    Adjust view using the User adapter

--- src/main/java/jhaturanga/views/leaderboard/LeaderboardView.java
+++ src/main/java/jhaturanga/views/leaderboard/LeaderboardView.java
@@ -11,7 +11,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import jhaturanga.controllers.leaderboard.LeaderboardController;
-import jhaturanga.model.user.User;
+import jhaturanga.model.leaderboard.adapter.LeaderboardUserAdapter;
 import jhaturanga.views.AbstractJavaFXView;
 import jhaturanga.views.pages.PageLoader;
 import jhaturanga.views.pages.Pages;
@@ -19,32 +19,37 @@ import jhaturanga.views.pages.Pages;
 public final class LeaderboardView extends AbstractJavaFXView {
 
     @FXML
-    private TableView<User> table;
+    private TableView<LeaderboardUserAdapter> table;
 
     @Override
     public void init() {
 
-        final TableColumn<User, String> nameColumn = new TableColumn<>("Username");
+        final TableColumn<LeaderboardUserAdapter, String> nameColumn = new TableColumn<>("Username");
         nameColumn.setCellValueFactory(u -> new SimpleStringProperty(u.getValue().getUsername()));
 
-        final TableColumn<User, Integer> winColumn = new TableColumn<>("Win");
+        final TableColumn<LeaderboardUserAdapter, Integer> winColumn = new TableColumn<>("Wins");
         winColumn.setCellValueFactory(u -> new SimpleIntegerProperty(u.getValue().getWinCount()).asObject());
 
-        final TableColumn<User, Integer> loseColumn = new TableColumn<>("Lose");
-        loseColumn.setCellValueFactory(u -> new SimpleIntegerProperty(u.getValue().getLostCount()).asObject());
-
-        final TableColumn<User, Integer> drawColumn = new TableColumn<>("Draw");
+        final TableColumn<LeaderboardUserAdapter, Integer> drawColumn = new TableColumn<>("Draws");
         drawColumn.setCellValueFactory(u -> new SimpleIntegerProperty(u.getValue().getDrawCount()).asObject());
 
-        final TableColumn<User, Integer> totalMatchColumn = new TableColumn<>("Total Match");
+        final TableColumn<LeaderboardUserAdapter, Integer> loseColumn = new TableColumn<>("Loses");
+        loseColumn.setCellValueFactory(u -> new SimpleIntegerProperty(u.getValue().getLostCount()).asObject());
+
+        final TableColumn<LeaderboardUserAdapter, Integer> totalMatchColumn = new TableColumn<>("Total Match");
         totalMatchColumn
                 .setCellValueFactory(u -> new SimpleIntegerProperty(u.getValue().getPlayedMatchCount()).asObject());
 
+        final TableColumn<LeaderboardUserAdapter, Integer> score = new TableColumn<>("Score");
+        score.setCellValueFactory(u -> new SimpleIntegerProperty(u.getValue().getScore()).asObject());
+
         table.getColumns().add(nameColumn);
         table.getColumns().add(winColumn);
-        table.getColumns().add(loseColumn);
         table.getColumns().add(drawColumn);
+        table.getColumns().add(loseColumn);
         table.getColumns().add(totalMatchColumn);
+        table.getColumns().add(score);
+
         try {
             this.table.setItems(new ObservableListWrapper<>(this.getLeaderboardController().getUsers()));
         } catch (IOException e) {
