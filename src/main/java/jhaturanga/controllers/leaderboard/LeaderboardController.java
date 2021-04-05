7a866b574527066ee09058612e665aa5f7fd0bf5
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2021-04-06 00:04:38 +0200

    Make the controller using the Leaderboard

--- src/main/java/jhaturanga/controllers/leaderboard/LeaderboardController.java
+++ src/main/java/jhaturanga/controllers/leaderboard/LeaderboardController.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import jhaturanga.controllers.Controller;
-import jhaturanga.model.user.User;
+import jhaturanga.model.leaderboard.adapter.LeaderboardUserAdapter;
 
 public interface LeaderboardController extends Controller {
 
@@ -13,5 +13,5 @@ public interface LeaderboardController extends Controller {
      * @return the list of all Users.
      * @throws IOException
      */
-    List<User> getUsers() throws IOException;
+    List<LeaderboardUserAdapter> getUsers() throws IOException;
 }
