ccb8088ab1476ec44e59f8afc80d8ca3ad9085b4
Tommaso Patriti <tommaso.patriti@studio.unibo.it>
2021-04-11 19:36:09 +0200

    a

---
+++ src/main/java/jhaturanga/views/Snippet.java
@@ -0,0 +1,6 @@
+package jhaturanga.views;
+
+public class Snippet {
+    Grafica volta al test dell'applicazione durante le prime fasi di sviluppo con fxml di base, timer e chiamate asincrone, salvataggio e replay delle partite, suoni della scacchiera, pezzi scacchiera personalizzabili, grafica applicazione personalizzabile, salvataggio delle impostazioni preferite dall'utente e relativa gestione file json, binding dell'applicazione(resizable), struttura ramificata dei css e caricamento di questi ultimi, PageLoader per caricamento, switch, reload e passaggio della scena, gestione di alcune directory di .jhaturanga
+}
+
