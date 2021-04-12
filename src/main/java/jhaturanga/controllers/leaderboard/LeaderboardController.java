dbcf209ac392d51817f3083ab9cc7208a90cdddb
Alessandro Mazzoli <Alemazzoli97@gmail.com>
2021-04-12 22:08:46 +0200

    Refactoring adding inheritDoc

--- src/main/java/jhaturanga/controllers/leaderboard/LeaderboardController.java
+++ src/main/java/jhaturanga/controllers/leaderboard/LeaderboardController.java
@@ -6,6 +6,9 @@ import java.util.List;
 import jhaturanga.controllers.Controller;
 import jhaturanga.model.leaderboard.adapter.LeaderboardUserAdapter;
 
+/**
+ * The controller for the leaderboard page view.
+ */
 public interface LeaderboardController extends Controller {
 
     /**
