86fc5c81db5169cc45f282d3ca440b706cafe79e
Tommaso Patriti <tommaso.patriti@studio.unibo.it>
2021-04-12 23:06:09 +0200

    replayTest refactoring

--- src/test/java/jhaturanga/test/model/replay/ReplayTest.java
+++ src/test/java/jhaturanga/test/model/replay/ReplayTest.java
@@ -2,21 +2,16 @@ package jhaturanga.test.model.replay;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
-
 import org.junit.jupiter.api.Test;
-
 import jhaturanga.commons.configurations.DirectoryConfigurations;
 import jhaturanga.model.board.BoardImpl;
 import jhaturanga.model.game.type.GameType;
-
 import jhaturanga.model.replay.ReplayBuilder;
 import jhaturanga.model.replay.ReplayData;
 import jhaturanga.model.replay.SavedReplay;
@@ -29,26 +24,18 @@ public class ReplayTest {
 
     private final ReplayData replay = new ReplayBuilder().blackUser(new UserImpl("nero", "nero", 0, 0, 0))
             .whiteUser(new UserImpl("bianco", "bianco", 0, 0, 0)).date(new Date()).gameType(GameType.CLASSIC)
-            .matchID("2323").boards(List.of(new BoardImpl(Set.of(), 8, 8))).build();
-
-    /**
-     * 
-     * @param path
-     * @throws IOException
-     */
-    private void deleteDirectory(final Path path) throws IOException {
-        Files.walk(path).sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
-    }
+            .matchID("test").boards(List.of(new BoardImpl(Set.of(), 8, 8))).build();
 
     @Test
     void replaySaved() throws IOException {
-        if (replaySaved.getAllBoards().size() != 0) {
-            deleteDirectory(Path.of(DirectoryConfigurations.HISTORY_DIRECTORY_PATH));
-        }
-        assertEquals(replaySaved.getAllBoards().size(), 0);
+
+        final int initalFile = replaySaved.getAllBoards().size();
         this.replaySaved.save(replay);
-        assertEquals(replaySaved.getAllBoards().size(), 1);
-        deleteDirectory(Path.of(DirectoryConfigurations.HISTORY_DIRECTORY_PATH));
+        assertEquals(replaySaved.getAllBoards().size(), initalFile + 1);
+        Files.deleteIfExists(
+                Path.of(DirectoryConfigurations.HISTORY_DIRECTORY_PATH + "/" + this.replay.getMatchID() + ".jhtr"));
+
+        assertEquals(replaySaved.getAllBoards().size(), initalFile);
 
     }
 
