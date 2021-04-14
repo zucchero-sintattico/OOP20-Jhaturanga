16f16fcacb00d72529144256f6a5b13585c640d2
Alessandro Mazzoli <Alemazzoli97@gmail.com>
2021-04-14 15:28:12 +0200

    added inheritDoc

--- src/main/java/jhaturanga/controllers/replay/ReplayControllerImpl.java
+++ src/main/java/jhaturanga/controllers/replay/ReplayControllerImpl.java
@@ -6,11 +6,18 @@ import jhaturanga.controllers.BasicController;
 import jhaturanga.model.board.Board;
 import jhaturanga.model.user.User;
 
+/**
+ * Basic implementation of the ReplayController.
+ *
+ */
 public class ReplayControllerImpl extends BasicController implements ReplayController {
 
     private static final int FIRST_BOARD_INDEX = 0;
     private int index;
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public final Optional<Board> getPreviousBoard() {
         return this.index > 0
@@ -18,6 +25,9 @@ public class ReplayControllerImpl extends BasicController implements ReplayContr
                 : Optional.empty();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public final Optional<Board> getNextBoard() {
         return this.index < this.getApplicationInstance().getReplay().get().getBoards().size() - 1
@@ -25,17 +35,26 @@ public class ReplayControllerImpl extends BasicController implements ReplayContr
                 : Optional.empty();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public final Board getFirstBoard() {
         return this.getApplicationInstance().getReplay().get().getBoards().get(FIRST_BOARD_INDEX);
 
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public final User getWhiteUser() {
         return this.getApplicationInstance().getReplay().get().getWhiteUser();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public final User getBlackUser() {
         return this.getApplicationInstance().getReplay().get().getBlackUser();
